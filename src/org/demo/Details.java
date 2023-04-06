76b3462a87b2f64766b56a39b73e17d9ca9f7d4b
soundaryaM16 <soundaryariya08@gmail.com>
2023-04-06 17:15:17 +0530

    example prg done

---
+++ src/org/demo/Details.java
@@ -0,0 +1,36 @@
+package org.demo;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Details {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Soundarya\\eclipse-workspace\\SeleniumD1\\driver\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		String driver1 = driver.getTitle();
+		System.out.println(driver1);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		
+		WebElement textEmail = driver.findElement(By.id("email"));
+		textEmail.sendKeys("somi@123");
+		
+		WebElement textPass = driver.findElement(By.id("pass"));
+		textPass.sendKeys("somi123");
+	
+		
+	}
+
+}
+
